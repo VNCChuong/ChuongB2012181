2c25cd3ead63d64e1753bcd3fa65bf22b6f0567b
VNCChuong <chuongvo900@gmail.com>
2022-09-22 21:24:10 +0700

    Cai dat xu ly loi

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -2,14 +2,15 @@ const express = require("express");
 const contacts = require("../controllers/contact.controller");
 const router = express.Router();
 
-router
-  .route('/')
+router.route('/')
   .get(contacts.findAll)
   .post(contacts.create)
   .delete(contacts.deleteAll);
-router.get('/favorite',contacts.findAllFavorite);
-router
-  .route('/:id')
+
+router.route('/favorite')
+    .get(contacts.findAllFavorite);
+
+router.route('/:id')
   .get(contacts.findOne)
   .put(contacts.update)
   .delete(contacts.delete);
